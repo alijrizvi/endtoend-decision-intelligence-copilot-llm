cd83d0581b6e506b0af5dd575991041fe3f692f7
Ali Rizvi <alij4relatives@yahoo.com>
2026-05-10 22:17:12 -0500

    Fix cloud file paths

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 def load_data():
 
-    df = pd.read_csv("/Users/alijazibrizvi/Documents/Data Analytics/Grand Project - End-to-End Data Analysis Copilot with LLMs/Data/df_final.csv")
+    df = pd.read_csv("Data/df_final.csv")
 
     return df
